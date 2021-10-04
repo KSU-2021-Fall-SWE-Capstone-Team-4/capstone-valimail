b5a43a8ef4c640851bb5ae527d4aef1d28881bf2
Jade <popkilj289@gmail.com>
2021-10-04 14:54:54 -0400

    Changed identity name in dane-jwe-jws test script

--- dane-jwe-jws_test_script.py
+++ dane-jwe-jws_test_script.py
@@ -2,7 +2,7 @@ from dane_jwe_jws.authentication import Authentication
 
 test_message = "hello yeye!!"
 prikey_path = 'prikey.txt'
-identity_name = "google.com"
+identity_name = "2fcf9ecce81b47d8b7884a7c158ea4a2.s1.eu.hivemq.cloud"
 signed = Authentication.sign(test_message, prikey_path, identity_name)
 print(signed)
 
