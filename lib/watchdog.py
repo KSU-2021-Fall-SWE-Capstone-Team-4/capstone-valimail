286d7afa29d49569656e3677a7c3ee7ffbd4fb6f
Jade <popkilj289@gmail.com>
2021-11-20 03:39:25 -0500

    Added unit tests for Watchdog

--- lib/watchdog.py
+++ lib/watchdog.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep
 import threading
 from threading import Thread
 
@@ -7,8 +7,10 @@ class Watchdog(Thread):
     def __init__(self):
         """
         Initializer for Watchdog thread.
+        Sets the stopping_event to False so that it can be set to True later in unit testing.
         """
         Thread.__init__(self)
+        self.stopping_event = False
 
     def run(self):
         """
@@ -21,6 +23,7 @@ class Watchdog(Thread):
             # Sets the value of main_thread_running, which keeps track of whether or not the main thread is, well, running.
             main_thread_running = False
             # Iterate through each thread and make sure that the main thread (MQTTListener) is still going.
+            # Iterate through each thread and make sure that the main thread (MQTTListener) is still going.
             for thread in threading.enumerate():
                 if type(thread) is threading._MainThread:
                     main_thread_running = True
@@ -28,5 +31,15 @@ class Watchdog(Thread):
             if not main_thread_running:
                 logging.critical('Main thread stopped running for some reason, exiting')
                 exit(-1)
+            # If the stopping event has been set, then stop.
+            if self.stopping_event:
+                return
             # Sleep for 1 second.
-            time.sleep(1)
+            sleep(1)
+
+    def emergency_stop(self):
+        """
+        Sets the stopping trigger, which should ideally terminate the loop.
+        Used exclusively in unit testing, just in case the test for run() fails.
+        """
+        self.stopping_event = True
