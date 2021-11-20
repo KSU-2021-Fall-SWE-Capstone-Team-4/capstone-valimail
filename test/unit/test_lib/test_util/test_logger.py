584a84ccbf0a849f3cd112c274cf08fbd86f96f0
Jade <popkilj289@gmail.com>
2021-11-20 03:47:08 -0500

    Added unit test for logger

---
+++ test/unit/test_lib/test_util/test_logger.py
@@ -0,0 +1,19 @@
+from unittest import mock, TestCase
+from lib.util import logger
+
+class TestLogger(TestCase):
+
+
+    @mock.patch("logging.debug")
+    def test_enable_debug_mode_can_run_after_basic_setup(self, m_d):
+        """lib.util.logger.enable_debug_mode.can_run_after_basic_setup"""
+        # Run basic_setup.
+        logger.basic_setup()
+        # Run enable_debug_mode.
+        logger.enable_debug_mode()
+
+
+
+# Running part.
+if __name__ == '__main__':
+    unittest.main()
